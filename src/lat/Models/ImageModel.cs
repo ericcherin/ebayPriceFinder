c1dbcaa3054b5c41468a39ecf6a655c37c0681f6
Eric Cherin <eric.cherin@gmail.com>
2016-04-19 23:56:48 -0400

    fin

--- src/lat/Models/ImageModel.cs
+++ src/lat/Models/ImageModel.cs
@@ -2,21 +2,17 @@
 using System.Collections.Generic;
 namespace lat.Models.Completed
 {
-
     public class ImageModel
     {
-        public int id { get; set; }
         public string url { get; set; }
-        public bool delete { get; set; }
 
         public ImageModel() { }
 
-        public ImageModel(string s)
-        {
-            url = s;
-        }
+        
 
     }
+
+
     public class SchoolImages
     {
         public IList<ImageModel> images { get; set; }
