5f8a6100f72cea2e920c3934a7a68cda98f2a8b7
Eric Cherin <eric.cherin@gmail.com>
2016-04-24 11:27:03 -0400

    .

---
+++ src/lat/Profiler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace lat
+{
+    public static class Profiler
+    {
+        private static Stopwatch sw = new Stopwatch();
+        private static List<Tuple<String, double>> times = new List<Tuple<String, double>>();
+        private static string lastTask;
+     
+        public static void start(string task)
+        {
+            lastTask = task;
+            sw.Start();
+        }
+        public static void newTask(string task)
+        {
+            stop();
+            start(task);
+        }
+
+        public static void stop()
+        {
+            sw.Stop();
+            //times.Add(new Tuple<string, double>(lastTask, sw.Elapsed.TotalSeconds));
+            Writer.appendString("" + lastTask + ", " + sw.Elapsed.TotalSeconds, "benchmark.txt");
+            sw.Reset();
+        }
+
+        public static void write()
+        {
+            Writer.appendBenchMark(times);
+        }
+
+        public static void clear()
+        {
+            Writer.clearFile("benchmark.txt");
+        }
+    }
+
+}
---
+++ src/lat/Writer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lat
+{
+    public static class Writer
+    {
+        
+        public static void appendString(string message)
+        {
+                System.IO.File.AppendAllText("log.txt", message + "\n");
+        }
+
+        public static void appendString(string message, string fileName)
+        {
+            System.IO.File.AppendAllText(fileName, message + "\n");
+        }
+
+        public static void appendBenchMark(List<Tuple<string, double>> times)
+        {
+            using (StreamWriter sw = System.IO.File.AppendText("benchmark.txt"))
+            {
+                foreach (Tuple<string, double> time in times)
+                {
+                    sw.Write(time + "\n");
+                }
+            }
+            
+        }
+
+        public static void clearFile(string filePath)
+        {
+            System.IO.File.WriteAllText(filePath, "");
+        }
+
+    }
+}
